0a1682a99522b7caee400a911c9501e7b6a33711
sfespinosa <scottjames.espinosa@gmail.com>
2020-06-09 00:06:56 -0700

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,39 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def paintings
+    Painting.all.select { |painting| painting.artist == self }
+  end
+
+  def galleries
+    self.paintings.collect { |painting| painting.gallery }
+  end
+
+  def cities
+    self.galleries.collect { |gallery| gallery.city }
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_experience
+    self.all.sum { |artist| artist.years_experience }
+  end
+
+  def Artist.most_prolific
+    self.all.max_by { |artist| artist.paintings.count / artist.years_experience }
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,22 @@
 require_relative '../config/environment.rb'
 
+julie = Artist.new("Julie", 4)
+nicole = Artist.new("Nicole", 2)
+regine = Artist.new("Regine", 1)
+melanie = Artist.new("Melanie", 2)
+jordan = Artist.new("Jordan", 10)
 
+sf_moma = Gallery.new("SF MOMA", "San Francisco")
+moco = Gallery.new("MOCO Museum", "Amsterdam")
+louvre = Gallery.new("The Louvre", "Paris")
 
+city = Painting.new("The City", 100, jordan, sf_moma)
+castle = Painting.new("The Castle", 500, melanie, sf_moma)
+trees = Painting.new("Trees", 20, julie, moco)
+butterfly = Painting.new("Butterfly", 55, regine, louvre)
+snail = Painting.new("Snail", 10, nicole, sf_moma)
+bridge = Painting.new("The Bridge", 150, jordan, moco)
+tower = Painting.new("The Tower", 124, jordan, louvre)
 
 binding.pry
 
